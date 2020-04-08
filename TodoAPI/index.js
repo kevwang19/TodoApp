e9dd2019def6e96fb058117dd700551571763c34
kevwang19 <52384003+kevwang19@users.noreply.github.com>
2020-04-08 11:20:23 -0400

    Add files via upload

--- TodoAPI/index.js
+++ TodoAPI/index.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const path = require("path");
 
+const app = express();
+const port = process.env.PORT || '8000';
+
+app.get("/", (req,res)=>{
+  res.status(200).send("Todo APIs")
+})
+
+app.get("/tasks", (req,res)=>{
+  var data = [
+    {
+      'name' : 'Feed the dog',
+      'status' : 'Incomplete',
+      'time' : '10.5'
+    },
+    {
+      'name' : 'Buy groceries',
+      'status' : 'Incomplete',
+      'time' : '11.5'
+    },
+    {
+      'name' : 'Check mail',
+      'status' : 'Incomplete',
+      'time' : '13.5'
+    },
+  ]
+
+  res.status(200).send(data)
+})
+
+app.listen(port, ()=>{
+  console.log('Listening to requests on http://locoalhost:${port}')
+})
